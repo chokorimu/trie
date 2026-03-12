c9f9616144902e07ff9d7d11a50b1edb3511b254
chokorimu <rezondavar@gmail.com>
2026-03-12 14:36:27 +0700

    commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node {
+    char value;
+    struct node* next;
+    bool isEnd;
+};
+
+void insert() {
+    
+}
+
+int main() {
+    struct node* head = NULL;
+
+}
