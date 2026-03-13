384739f3b6b11ce200b1077cbd86281ea4256e83
chokorimu <rezondavar@gmail.com>
2026-03-13 19:02:19 +0700

    add insert() and search()

--- main.c
+++ main.c
@@ -1,18 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define ARR_SIZE 26
 
 struct node {
-    char value;
-    struct node* next;
+    struct node* next[ARR_SIZE];
     bool isEnd;
 };
 
-void insert() {
-    
+struct node* allocate() {
+    struct node* newNode = malloc(sizeof(*newNode));
+    for(int i=0; i<ARR_SIZE; i++) {
+        newNode->next[i] = NULL;
+    }
+    newNode->isEnd = false;
+    return newNode;
+}
+
+void insert(struct node** head, char* newValue) {
+    if((*head) == NULL) {
+        *head = allocate();
+    }
+    struct node* cursor = *head;
+    for(int i=0; newValue[i]!='\0'; i++) {
+        int index = newValue[i]-'a';
+        if(cursor->next[index] == NULL) {
+            cursor->next[index] = allocate();
+        }
+        cursor=cursor->next[index];
+    }
+    cursor->isEnd = true;
+}
+
+bool search(struct node** head, char* value) {
+    if (*head == NULL) {
+        return false;
+    }
+    struct node* cursor = *head;
+    for(int i=0; value[i]!='\0'; i++) {
+        int index = value[i]-'a';
+        if(cursor->next[index] == NULL) {
+            return false;
+        }
+        cursor=cursor->next[index];
+    }
+    if(cursor->isEnd == true) {
+        return true;
+    }
+    return false;
+}
+
+void check(struct node** head, char* value) {
+    bool found = search(head, value);
+    if(found) {
+        printf("\"%s\" ditemukan\n", value);
+    } else {
+        printf("\"%s\" tidak ditemukan\n", value);
+    }
 }
 
 int main() {
     struct node* head = NULL;
+    insert(&head, "kuda");
+    insert(&head, "kudalaut");
+    check(&head, "kuda");
+    check(&head, "kudala");
 
+    return 0;
 }
